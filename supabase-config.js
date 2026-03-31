2fc8e98140acee28035b8d7b6898946a12ba74be
JohnPaul-223 <johnpaulcanopen@gmail.com>
2026-03-31 09:24:47 +0800

    Update Supabase configuration URL and anonKey

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 window.SUPABASE_CONFIG = {
-    url: "https://olafypkodcfcaceqhafe.supabase.co",
-    anonKey: "sb_publishable_688cYwooaSuhv38gEu6BGg_wyinh4zB",
+    url: "https://bkxwtekxshfrvvflpaed.supabase.co",
+    anonKey: "sb_publishable_XyV_8TP2Lg86ss-XMdr_Dg_u4ZR7SdW",
     employeeLinkEmailFunctionName: "",
     employeeLinkEmailWebhookUrl: ""
 };
